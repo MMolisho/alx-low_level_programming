b8eb125fc800a645ff267bc6ae226c927939f4df
MMolisho <myogamolisho@gmail.com>
2023-02-17 21:12:26 -0800

    software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
+#include  <stdio.h>
 /**
  * main - main function
  * Return: not always 0
  */
 int main(void)
 {
-	int i = 0;
+	int c = 0;
 	int f_d;
 	int l_d;
 
-	int i2;
+	int c2;
 	int f_d2;
-	int l_d2
+	int l_d2;
 
-	while (i <= 98)
+	while (c <= 98)
 	{
-		f_d = (i / 10 + '0');
-		l_d = (i % 10 + '0');
-		i2 = 0;
-		while (i2 <= 99)
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
 		{
-			f_d2 = (i2 / 10 + '0');
-			l_d2 = (i2 % 10 + ;0; );
+			f_d2 = (c2 / 10 + '0');
+			l_d2 = (c2 % 10 + '0');
 
-			if (i < i2)
+			if (c < c2)
 			{
 				putchar(f_d);
 				putchar(l_d);
@@ -31,17 +31,16 @@ int main(void)
 				putchar(f_d2);
 				putchar(l_d2);
 
-				if (i != 98)
+				if (c != 98)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-			i2++;
+			c2++;
 		}
-		i++
+		c++;
 	}
-
-}	
-
-
+	putchar('\n');
+	return (0);
+}
