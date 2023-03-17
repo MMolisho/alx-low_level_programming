c8ccdd22e13c84df2b03f8498013d64fd26a2443
MMolisho <myogamolisho@gmail.com>
2023-03-16 17:15:47 -0700

    array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,10 +15,9 @@ int *array_range(int min, int max)
 	int i, size;
 
 	if (min > max)
-		return (NULL);
+	return (NULL);
 	size = max - min + 1;
 	ptr = malloc(sizeof(int) * size);
-
 	if (ptr == Null)
 		return (NULL);
 	for (i = 0; min <= max; i++)
